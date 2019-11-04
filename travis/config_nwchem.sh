e0421dd2523e94c73800b357b0a1cd4cef90a009
edoapra <edoardo.apra@gmail.com>
2019-11-04 13:38:20 -0800

    use release tarball instead of git

--- travis/config_nwchem.sh
+++ travis/config_nwchem.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -f
 # source env. variables
+TARBALL=https://github.com/nwchemgit/nwchem/releases/download/v7.0.0-beta1/nwchem-7.0.0-release.revision-5bcf0416-src.2019-11-01.tar.bz2
+if [[ !-z "$TARBALL" ]]; then
+    cd $TRAVIS_BUILD_DIR/..
+    mv nwchem nwchem.git
+    curl $TARBALL -o nwchem.tar.gz
+    tar xzf nwchem.tar.gz
+    ln -sf nwchem-7.0.0 nwchem
+fi
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
  ls -lrt $TRAVIS_BUILD_DIR|tail -2
  cd $TRAVIS_BUILD_DIR/src
      make nwchem_config
+if [[ -z "$TARBALL" ]]; then
  if [[ "$USE_64TO32" == "y" ]]; then
      echo " CONVERSION 64_to_32"
      make 64_to_32 >& 6log &
  fi
+fi
  env
