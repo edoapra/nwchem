ba1fb887730814de98f473c9b4831db9e79caec5
edoapra <edoardo.apra@gmail.com>
2019-11-04 15:06:33 -0800

    bash fixes

--- travis/config_nwchem.sh
+++ travis/config_nwchem.sh
@@ -1,13 +1,14 @@
-#!/bin/bash -f
-# source env. variables
+#!/bin/bash
 TARBALL=https://github.com/nwchemgit/nwchem/releases/download/v7.0.0-beta1/nwchem-7.0.0-release.revision-5bcf0416-src.2019-11-01.tar.bz2
-if [[ !-z "$TARBALL" ]]; then
+if [[ ! -z "$TARBALL" ]]; then
     cd $TRAVIS_BUILD_DIR/..
+    pwd
     mv nwchem nwchem.git
-    curl $TARBALL -o nwchem.tar.gz
-    tar xzf nwchem.tar.gz
+    curl -LJ $TARBALL -o nwchem.tar.bz2
+    tar xjf nwchem.tar.bz2
     ln -sf nwchem-7.0.0 nwchem
 fi
+# source env. variables
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
  ls -lrt $TRAVIS_BUILD_DIR|tail -2
  cd $TRAVIS_BUILD_DIR/src
