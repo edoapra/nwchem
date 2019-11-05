84ed5252499d9246fd9cd1e347b3580ba08857d4
edoapra <edoardo.apra@gmail.com>
2019-11-04 18:32:15 -0800

    need to swap tarball travis with git one

--- travis/config_nwchem.sh
+++ travis/config_nwchem.sh
@@ -8,6 +8,8 @@ if [[ ! -z "$TARBALL" ]]; then
     curl -LJ $TARBALL -o nwchem.tar.bz2
     tar xjf nwchem.tar.bz2
     ln -sf nwchem-7.0.0 nwchem
+    mv nwchem/travis nwchem/travis.tarball
+    cp -r nwchem.git/travis nwchem/.
 fi
  ls -lrt $TRAVIS_BUILD_DIR|tail -2
  cd $TRAVIS_BUILD_DIR/src
