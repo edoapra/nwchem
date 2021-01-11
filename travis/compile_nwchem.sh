dd3645b6e91864a8e6e9545959447fe39d3ae48a
edoapra <edoardo.apra@gmail.com>
2021-01-11 12:00:49 -0800

    chasing the QA aump2 slowdown

--- travis/compile_nwchem.sh
+++ travis/compile_nwchem.sh
@@ -86,7 +86,7 @@ fi
      cd $TRAVIS_BUILD_DIR/src/64to32blas 
      make
      # recompile to find teh source of the QA aump2 slowdown
-     cd mp2 ; make clean ;make FOPTIMIZE="-O2 -fno-aggressive-loop-optimizations  -ffast-math" FDEBUG="-O0 -g" -j3
+     cd mp2_grad ; make clean ;make FOPTIMIZE="-O2 -fno-aggressive-loop-optimizations  -ffast-math" FDEBUG="-O0 -g" -j3
      cd $TRAVIS_BUILD_DIR/src
      $TRAVIS_BUILD_DIR/contrib/getmem.nwchem 1000
  fi
