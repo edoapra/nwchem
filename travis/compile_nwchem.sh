69dd446936134b6cedf61f35388097d340a9394d
edoapra <edoardo.apra@gmail.com>
2021-01-12 12:56:20 -0800

    ffgffgO0f

--- travis/compile_nwchem.sh
+++ travis/compile_nwchem.sh
@@ -47,7 +47,8 @@ else
 		FOPT="-O2  -ffast-math"
 	    else
 		#                FDOPT=" -fno-tree-dominator-opts  -finline-functions -O2 -g  -fno-aggressive-loop-optimizations -fno-tree-dominator-opts  -g -O"
-		FDOPT=" -O -g -fno-aggressive-loop-optimizations"
+#		FDOPT=" -O -g -fno-aggressive-loop-optimizations"
+		FDOPT=" -fno-tree-dominator-opts  -finline-functions -g -fno-aggressive-loop-optimizations -fno-tree-dominator-opts  -g -O0 -fno-aggressive-loop-optimizations"
 #		FDOPT=" -O2 -g -fno-aggressive-loop-optimizations -g -O"
 		#		FOPT="-O2 -fno-aggressive-loop-optimizations  -ffast-math"
 #		FOPT="-fno-tree-dominator-opts  -finline-functions -O2  -Wuninitialized -fno-aggressive-loop-optimizations -O3  -mfpmath=sse  -fno-tree-dominator-opts  -ffast-math  -fprefetch-loop-arrays  -ftree-vectorize    -mtune=native "
@@ -88,7 +89,7 @@ fi
      export MAKEFLAGS=-j3
      echo    "$FOPT$FDOPT"
 if [[ -z "$TRAVIS_HOME" ]]; then
-    make V=0 FOPTIMIZE="$FOPT" FDEBUG="$FDOPT"  -j3
+    make V=1 FOPTIMIZE="$FOPT" FDEBUG="$FDOPT"  -j3
 else
     ../travis/sleep_loop.sh make V=1 FOPTIMIZE="$FOPT" FDEBUG="$FDOPT"  -j3
 fi
