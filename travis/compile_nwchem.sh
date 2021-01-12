46af7c62b0853831599f83b7669a0f1d959ec4da
edoapra <edoardo.apra@gmail.com>
2021-01-11 23:15:27 -0800

    change fdebug

--- travis/compile_nwchem.sh
+++ travis/compile_nwchem.sh
@@ -40,15 +40,17 @@ else
     elif [[ "$FC" == "flang" ]] ; then
 	export BUILD_MPICH=1
     else
-	if [[ -z "$TRAVIS_HOME" ]]; then
-	    echo ' do not modify compiler options when using github actions '
-	else
+#	if [[ -z "$TRAVIS_HOME" ]]; then
+#	    echo ' do not modify compiler options when using github actions '
+#	else
 	    if [[ "$FC" == "flang" ]] ; then
 		FOPT="-O2  -ffast-math"
 	    else
+                FDOPT=" -fno-tree-dominator-opts  -finline-functions -O2 -g  -fno-aggressive-loop-optimizations -fno-tree-dominator-opts  -g -O"
 		FOPT="-O2 -fno-aggressive-loop-optimizations  -ffast-math"
+#		FOPT+="
 	    fi
-	fi
+#	fi
     fi
 fi    
  if [[ "$os" == "Darwin" ]]; then 
