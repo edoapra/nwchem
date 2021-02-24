0112597b4886f541367baac68d69b4098aab5f0b
edoapra <edoardo.apra@gmail.com>
2021-02-23 20:03:11 -0800

    lmod

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -83,7 +83,7 @@ fi
 	which flang
     fi
     if [[ "$FC" == "nvfortran" ]]; then
-	sudo apt-get16511 -y install lmod
+	sudo apt-get -y install lmod
 	wget https://developer.download.nvidia.com/hpc-sdk/21.2/nvhpc-21-2_21.2_amd64.deb
 	wget https://developer.download.nvidia.com/hpc-sdk/21.2/nvhpc-2021_21.2_amd64.deb
 	sudo dpkg -i nvhpc-21-2_21.2_amd64.deb nvhpc-2021_21.2_amd64.deb
