6ee5585e58ae884f668cd3cce38c587e311c9395
edoapra <edoardo.apra@gmail.com>
2021-04-15 12:59:17 -0700

    used current dev for OpenBLAS [do_extra_archs]

--- src/libext/openblas/build_openblas.sh
+++ src/libext/openblas/build_openblas.sh
@@ -2,16 +2,23 @@
 set -v
 arch=`uname -m`
 VERSION=0.3.13
-if [ -f  OpenBLAS-${VERSION}.tar.gz ]; then
-    echo "using existing"  OpenBLAS-${VERSION}.tar.gz
+COMMIT=974acb39ff86121a5a94be4853f58bd728b56b81
+BRANCH=develop
+#if [ -f  OpenBLAS-${VERSION}.tar.gz ]; then
+#    echo "using existing"  OpenBLAS-${VERSION}.tar.gz
+if [ -f  OpenBLAS-$COMMIT.zip ]; then
+    echo "using existing"  OpenBLAS-${COMMIT}.zip
 else
     rm -rf OpenBLAS*
-    curl -L https://github.com/xianyi/OpenBLAS/archive/v${VERSION}.tar.gz -o OpenBLAS-${VERSION}.tar.gz
+    curl -L https://github.com/xianyi/OpenBLAS/archive/$COMMIT.zip -o OpenBLAS-$COMMIT.zip
+#    curl -L https://github.com/xianyi/OpenBLAS/archive/v${VERSION}.tar.gz -o OpenBLAS-${VERSION}.tar.gz
 fi
 # patch for avx2 detection
-tar xzf OpenBLAS-${VERSION}.tar.gz
-ln -sf OpenBLAS-${VERSION} OpenBLAS
-cd OpenBLAS-${VERSION}
+unzip -n -q OpenBLAS-$COMMIT.zip
+ln -sf OpenBLAS-$COMMIT OpenBLAS
+#tar xzf OpenBLAS-${VERSION}.tar.gz
+#ln -sf OpenBLAS-${VERSION} OpenBLAS
+cd OpenBLAS
 # patch for apple clang -fopenmp
 patch -p0 < ../clang_omp.patch
 if [[  -z "${FORCETARGET}" ]]; then
